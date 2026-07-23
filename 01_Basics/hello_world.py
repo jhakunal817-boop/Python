35f751807378c5fff2fe481033dc9cb6240f5817
Kunal Jha <jhakunal817@gmail.com>
2026-07-23 07:09:35 +0545

    Fix spelling and enhance hello_world.py output
    Corrected spelling of 'World' and added additional print statements.

--- 01_Basics/hello_world.py
+++ 01_Basics/hello_world.py
@@ -1,7 +1,11 @@
-# Simple Python code to print/display HelloWorld
+# Hello_World.py
 
-# Program: 
-print("Hello Wrold")
+#Program:
+print("Hello, World!")
+print("Welcome to Python Programming!")
+print("This is my first Python program.")
 
-# Output:
-# Hello World
+#Output: 
+# Hello, World!
+# Welcome to Python Programming!
+# This is my first python program.
