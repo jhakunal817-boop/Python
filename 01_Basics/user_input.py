6dbf8847ff9675260a84e0c3cee961a64fe25878
Kunal Jha <jhakunal817@gmail.com>
2026-07-24 07:34:16 +0545

    Modify height output format in user input script
    Updated height output to include 'feet'.

--- 01_Basics/user_input.py
+++ 01_Basics/user_input.py
@@ -1,7 +1,7 @@
 # user_input.py
 
 # Taking users input
-
+print("User Input:-")
 name = input("Enter your name: ")   #String Value
 age = int(input("Enter your age: "))   #Integer Value
 height = float(input("Enter your height: "))   #Floating Value
@@ -9,11 +9,20 @@ height = float(input("Enter your height: "))   #Floating Value
 print("\n----- User Information -----")
 print("Name:", name)
 print("Age:", age)
-print("Height:", height)
+print("Height:", height, "feet")
 
 print("\nHello", name + "!")
 print("Next year you will be", age + 1, "years old.")
 
 # Output:
+# User Input:-
 # Enter your name: Kunal
 # Enter your age: 17
+# Enter your height: 5.7 
+# -----User Information-----
+# Name: Kunal
+# Age: 17
+# Height: 5.7 feet
+# /n made a break or like a statement that skips one line.
+# Hello Kunal!
+# Next year you will be 18 years old.
